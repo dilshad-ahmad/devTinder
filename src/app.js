27bfbdbd1cc8231fc64fff426124de800e7d47b2
Dilshad Ahmad <laddu4591@gmail.com>
2025-07-02 12:26:42 +0530

    playing with routes

--- src/app.js
+++ src/app.js
@@ -5,20 +5,39 @@ const app = express()
 
 
 
-app.use("/hello", (req,res) => {
-    res.send("Hello from server!")
-}) ;
+app.get("/user",(req,res) => {
+    res.send({firstname:"Dilshad",lastname:"Ahmad"})
+});
 
+app.post("/user", (req,res) => {
+    res.send("Data sucessfully saved in a database")
+
+});
+
+app.delete("/user", (req,res) => {
+    res.send("Data are Deleted ")
+});
+
+//PUT is used in HTTP to update whole data 
+app.put("/user/data", (req,res) => {
+    res.send({firstname:"Naushad",lastname:"Ahmad"})
+});
+
+//PATCH is used to update partial data 
+app.patch("/user/data",(req,res) => {
+    console.log(req.body);
+    res.send({firstname:"Naushad",lastname:"Ali"})
+})
+
+
+
+//This will match all the all HTTP method API call to /test 
 app.use("/test", (req,res) => {
     res.send("Test is checked ")
 
 });
 
-app.use("/" ,(req,res) => {
-    res.send("Welcome to dashboard from Laddu");
-    next();
 
-})
 
 
 
